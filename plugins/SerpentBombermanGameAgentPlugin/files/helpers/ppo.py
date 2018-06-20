307571a013814640e1579464c30d68844d190d62
Jorge Enrique Espinoza Muñoz <jeespinozam@pucp.pe>
2018-06-19 23:23:04 -0500

    new version

--- plugins/SerpentBombermanGameAgentPlugin/files/helpers/ppo.py
+++ plugins/SerpentBombermanGameAgentPlugin/files/helpers/ppo.py
@@ -3,6 +3,7 @@ from tensorforce.agents import PPOAgent
 from serpent.utilities import SerpentError
 
 import numpy as np
+import os
 
 # This file is borrowed from SerpentAIsaacGameAgentPlugin:
 # https://github.com/SerpentAI/SerpentAIsaacGameAgentPlugin/blob/master/files/helpers/ppo.py
@@ -23,30 +24,18 @@ class SerpentPPO:
 
         actions_spec = {"type": "int", "num_actions": len(self.game_inputs)}
 
-        summary_spec = {
-            "directory": "./board/",
-            "steps": 50,
-            "labels": [
-                "configuration",
-                "gradients_scalar",
-                "regularization",
-                "inputs",
-                "losses",
-                "variables"
-            ]
-        }
-
-		network_spec = [
+        network_spec = [
             {"type": "conv2d", "size": 16, "window": 2, "stride": 1},
             {"type": "flatten"},
             {"type": "dense", "size": 64},
-            {"type": "dense", "size": 32}
+            #{"type": "dense", "size": 32}
         ]
 
         self.agent = PPOAgent(
             states=states_spec,
             actions=actions_spec,
             network=network_spec,
+
             batched_observe=256,
             batching_capacity=1000,
             # BatchAgent
@@ -54,39 +43,37 @@ class SerpentPPO:
             # PPOAgent
             step_optimizer=dict(
                 type='adam',
-                learning_rate=1e-3
+                learning_rate=1e-4
             ),
             optimization_steps=10,
             # Model
-            scope='ppo',
-            discount=0.97,
+            scope='ppo'
+                #discount=0.97,
             # DistributionModel
-            distributions=None,
-            entropy_regularization=0.01,
+                #distributions=None,
+                #entropy_regularization=0.01,
             # PGModel
-            baseline_mode=None,
-            baseline=None,
-            baseline_optimizer=None,
-            gae_lambda=None,
+                #baseline_mode=None,
+                #baseline=None,
+                #baseline_optimizer=None,
+                #gae_lambda=None,
             # PGLRModel
-            likelihood_ratio_clipping=None,
+                #likelihood_ratio_clipping=None,
             #summary_spec=summary_spec,
             #distributed_spec=None,
             # More info
-            device=None,
+                #device=None,
             #session_config=None,
-            saver=None,
-            variable_noise=None,
+                #saver=None,
+                #variable_noise=None,
             #states_preprocessing_spec=None,
             #explorations_spec=None,
             #reward_preprocessing_spec=None,
-
-            summarizer=None,
-            execution=None,
-            actions_exploration=None,
-            update_mode=None,
-            memory=None,
-            subsampling_fraction=0.1
+                #execution=None,
+                #actions_exploration=None,
+                #update_mode=None,
+                #memory=None,
+                #subsampling_fraction=0.1
         )
 
     def generate_action(self, game_frame_buffer):
@@ -97,10 +84,8 @@ class SerpentPPO:
 
         # Get prediction from agent, execute
         action = self.agent.act(states)
-        #print(action)
         label = self.game_inputs_mapping[action]
-        #print(label)
-        #print(self.game_inputs[label])
+
         return action, label, self.game_inputs[label]
 
     def observe(self, reward=0, terminal=False):
@@ -115,7 +100,8 @@ class SerpentPPO:
         return mapping
 
     def save_model(self):
-        self.ppo_agent.agent.save_model(directory=os.path.join(os.getcwd(), "datasets", "aisaac", "ppo_model"), append_timestep=False)
+        self.agent.save_model(directory=os.path.join(os.getcwd(), "datasets", "bomberman", "ppo_model"), append_timestep=False)
 
-    def load_model(self):
-        self.ppo_agent.agent.restore_model(directory=os.path.join(os.getcwd(), "datasets", "aisaac"))
+    def restore_model(self):
+        self.agent.restore_model(directory=os.path.join(os.getcwd(), "datasets", "bomberman"))
+		
