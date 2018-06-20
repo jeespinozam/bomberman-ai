c29e60edf82dcba00c93f386138c6120199f068f
Jorge Enrique Espinoza Muñoz <jeespinozam@pucp.pe>
2018-06-19 23:29:01 -0500

    new version

--- plugins/SerpentBombermanGamePlugin/files/serpent_Bomberman_game.py
+++ plugins/SerpentBombermanGamePlugin/files/serpent_Bomberman_game.py
@@ -1,6 +1,6 @@
 from serpent.game import Game
 
-from .api.api import BombermanAPI
+from .api.api import TestAPI
 
 from serpent.utilities import Singleton
 
@@ -9,44 +9,108 @@ from serpent.game_launchers.web_browser_game_launcher import WebBrowser
 
 class SerpentBombermanGame(Game, metaclass=Singleton):
 
-    def __init__(self, **kwargs):
-        kwargs["platform"] = "web_browser"
+	def __init__(self, **kwargs):
+		kwargs["platform"] = "web_browser"
 
-        kwargs["window_name"] = "Safari"
+		kwargs["window_name"] = "Safari"
 
-        kwargs["url"] = "http://0.0.0.0:8000"
-        kwargs["browser"] = WebBrowser.DEFAULT
+		kwargs["url"] = "http://0.0.0.0:8000"
+		kwargs["browser"] = WebBrowser.DEFAULT
 
-        super().__init__(**kwargs)
+		super().__init__(**kwargs)
 
-        self.api_class = BombermanAPI
-        self.api_instance = None
+		self.api_class = TestAPI
+		self.api_instance = None
 
-    @property
-    def screen_regions(self):
-        regions = {
-            "GAME_REGION": (0, 0, 480, 549), ##545x416
-            "GAME_OVER_REGION": (160,160, 225, 404),
-            "WIN_REGION": (175,130, 220, 421),
-        }
+	@property
+	def screen_regions(self):
 
-        return regions
+		#t
+		dic_offset = {
+			"WINDOWS_CHROME": {
+				"top": 81,
+				"left": 5
+			}
+		}
 
-    @property
-    def ocr_presets(self):
-        presets = {
-            "SAMPLE_PRESET": {
-                "extract": {
-                    "gradient_size": 1,
-                    "closing_size": 1
-                },
-                "perform": {
-                    "scale": 10,
-                    "order": 1,
-                    "horizontal_closing": 1,
-                    "vertical_closing": 1
-                }
-            }
-        }
+		offset = dic_offset["WINDOWS_CHROME"]
 
-        return presets
+		regions = {
+			"GAME_REGION": (offset["top"], offset["left"], 416 + offset["top"], 544 + offset["left"]), #544x416
+			"GAME_OVER_REGION": (118 + offset["top"], 163 + offset["left"], 151 + offset["top"], 383 + offset["left"]), #220x33 - 163,118
+			"WIN_REGION": (118 + offset["top"], 171 + offset["left"], 149 + offset["top"], 372 + offset["left"]), # 201x31  - 171,118
+		}
+
+		return regions
+
+	@property
+	def ocr_presets(self):
+		presets = {
+			"SAMPLE_PRESET": {
+				"extract": {
+					"gradient_size": 1,
+					"closing_size": 1
+				},
+				"perform": {
+					"scale": 10,
+					"order": 1,
+					"horizontal_closing": 1,
+					"vertical_closing": 1
+				}
+			}
+		}
+
+		return presets
+
+# from serpent.game import Game
+#
+# from .api.api import BombermanAPI
+#
+# from serpent.utilities import Singleton
+#
+# from serpent.game_launchers.web_browser_game_launcher import WebBrowser
+#
+#
+# class SerpentBombermanGame(Game, metaclass=Singleton):
+#
+#     def __init__(self, **kwargs):
+#         kwargs["platform"] = "web_browser"
+#
+#         kwargs["window_name"] = "Safari"
+#
+#         kwargs["url"] = "http://0.0.0.0:8000"
+#         kwargs["browser"] = WebBrowser.DEFAULT
+#
+#         super().__init__(**kwargs)
+#
+#         self.api_class = BombermanAPI
+#         self.api_instance = None
+#
+#     @property
+#     def screen_regions(self):
+#         regions = {
+#             "GAME_REGION": (0, 0, 480, 549), ##545x416
+#             "GAME_OVER_REGION": (160,160, 225, 404),
+#             "WIN_REGION": (175,130, 220, 421),
+#         }
+#
+#         return regions
+#
+#     @property
+#     def ocr_presets(self):
+#         presets = {
+#             "SAMPLE_PRESET": {
+#                 "extract": {
+#                     "gradient_size": 1,
+#                     "closing_size": 1
+#                 },
+#                 "perform": {
+#                     "scale": 10,
+#                     "order": 1,
+#                     "horizontal_closing": 1,
+#                     "vertical_closing": 1
+#                 }
+#             }
+#         }
+#
+#         return presets
